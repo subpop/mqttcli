b88e5f40eeb2b002c7a3db10b082b38f487ff68c
Link Dupont <link@sub-pop.net>
2022-02-26 21:28:54 -0500

    fix: generate client ID compliant with MQTT spec

--- flag.go
+++ flag.go
@@ -2,7 +2,6 @@ package mqttcli
 
 import (
 	"flag"
-	"log"
 	"os"
 	"path/filepath"
 
@@ -27,11 +26,6 @@ var (
 // GlobalFlagSet returns a new flag set configured with flags common to publish
 // and subscribe clients.
 func GlobalFlagSet(name string, errorHandling flag.ErrorHandling) *flag.FlagSet {
-	hostname, err := os.Hostname()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	if name == "" {
 		name = filepath.Base(os.Args[0])
 	}
@@ -40,7 +34,7 @@ func GlobalFlagSet(name string, errorHandling flag.ErrorHandling) *flag.FlagSet 
 
 	fs.StringVar(&Broker, "broker", "", "broker address (should be in the form of a `URL`)")
 	fs.Var(&Topics, "topic", "topic to publish or subscribe to\n(can be specified multiple times)")
-	fs.StringVar(&ClientID, "client-id", hostname+"-"+randomString(6), "unique identifier for this client")
+	fs.StringVar(&ClientID, "client-id", randomString(23), "unique identifier for this client")
 	fs.StringVar(&Username, "username", "", "authenticate with a username")
 	fs.StringVar(&Password, "password", "", "authenticate with a password")
 	fs.StringVar(&CARoot, "ca-root", "", "path to a `file` containing CA certificates")
